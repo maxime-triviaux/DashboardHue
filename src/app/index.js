e6f6dcbaabc70deffb795ed5bfde2325fe29af96
Maxime TRIVIAUX <triviauxm@gmail.com>
2018-02-11 22:28:51 +0100

    Move logic code in the view

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,6 @@ var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('Backbone');
 var Router = require('./router.js');
-var BridgeModel = require('./models/BridgeModel.js');
 
 
 require('hammerjs');
@@ -10,16 +9,5 @@ require('materialize-css');
 
 
 $(document).ready(function() {
-
-  var bridge = new BridgeModel({
-    ip: '192.168.1.10',
-    token: "SzeJNNH4ZEB636mc4NxuBdDKcLMkgZ61TYyCxyS3"
-  });
-  bridge.getLights(function(lights){
-    console.log(lights);
-  });
-
   new Router();
-
-
 });
--- src/app/views/DefaultView.js
+++ src/app/views/DefaultView.js
@@ -3,6 +3,7 @@ var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('Backbone');
 //var template = require('../templates/home.hbs');
+var BridgeModel = require('../models/BridgeModel.js');
 
 
 
@@ -23,6 +24,14 @@ module.exports = Backbone.View.extend({
     // var compiled = template({tasks: this.tasks.toJSON()});
     // $(this.el).html(compiled);
     // return this;
+    var bridge = new BridgeModel({
+      ip: '192.168.1.10',
+      token: "SzeJNNH4ZEB636mc4NxuBdDKcLMkgZ61TYyCxyS3"
+    });
+    bridge.getLights(function(lights){
+      console.log(lights);
+    });
+
   }
 
 
